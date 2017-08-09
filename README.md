cad38c53168737d0fb982399eca7f0036ef29f2d
杨浩 <takeseem@gmail.com>
2017-08-09 10:11:31 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,11 @@
+# 塔星科技
+
+http://www.takeseem.com
+
+## 在线演示
+
+http://www.takeseem.com
+
+## Licence
+
+版权归原作者所有，欢迎演绎和分发，但必须注明来源以及来源地址。
---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+  baseurl=window.baseurl || "";
+  var tmp1 = baseurl.indexOf('/');
+  if (tmp1 != -1 && tmp1 > 0 && baseurl.charAt(tmp1 -1) == ':') tmp1 = baseurl.indexOf('/', tmp1 + 2);
+  if (tmp1 != -1) baseurl = baseurl.substring(tmp1);
+  
+  $('a.blog-button').click(function() {
+    // If already in blog, return early without animate overlay panel again.
+    if (location.hash && location.hash == "#blog") return;
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
+    $('.main-post-list').removeClass('hidden');
+    currentWidth = $('.panel-cover').width();
+    if (currentWidth < 960) {
+      $('.panel-cover').addClass('panel-cover--collapsed');
+    } else {
+      $('.panel-cover').css('max-width',currentWidth);
+      $('.panel-cover').animate({'max-width': '700px', 'width': '30%'}, 400, swing = 'swing', function() {} );
+    }
+  });
+
+  if (window.location.hash && window.location.hash == "#blog") {
+    $('.panel-cover').addClass('panel-cover--collapsed');
+    $('.main-post-list').removeClass('hidden');
+  }
+
+  tagPath = baseurl + "/tag/";
+  if (window.location.pathname.substring(0, tagPath.length) == tagPath) {
+    $('.panel-cover').addClass('panel-cover--collapsed');
+  }
+
+  $('.btn-mobile-menu__icon').click(function() {
+    if ($('.navigation-wrapper').css('display') == "block") {
+      $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
+        $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+      });
+      $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
+
+    } else {
+      $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
+    }
+    $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
+  });
+
+  $('.navigation-wrapper .blog-button').click(function() {
+    if ($('.navigation-wrapper').css('display') == "block") {
+      $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
+        $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+      });
+
+      $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
+    }
+    
+    $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
+  });
+});
